abada9d84519829747144d4d9f1187b0ee3363c1
Denis Shimansky <brodsm1487@gmail.com>
2022-12-14 00:32:28 +0200

    Create User.cs

---
+++ MyTodo.Storage/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace MyTodo.Storage.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string Username { get; set; } = $"user_{ DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() }";
+        public string Email { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+
+        public ICollection<TodoList>? TodoLists { get; set; }
+    }
+}
