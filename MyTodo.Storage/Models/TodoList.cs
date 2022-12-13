6ee87301808dd4c3a4306312db7e45fd5706fe0a
Denis Shimansky <brodsm1487@gmail.com>
2022-12-14 00:32:23 +0200

    rename task lists to todo lists

--- MyTodo.Storage/Models/TodoList.cs
+++ MyTodo.Storage/Models/TodoList.cs
@@ -1,11 +1,15 @@
 ﻿namespace MyTodo.Storage.Models
 {
-    public class TaskList
+    public class TodoList
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
         public int AuthorId { get; set; }
         public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+
+
+        public ICollection<TodoListItem>? Tasks { get; set;}
+        public User? Author { get; set; }
     }
 }
