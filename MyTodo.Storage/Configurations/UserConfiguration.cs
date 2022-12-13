a290a9c44a7392985df8d44cf9de07cd2b203024
Denis Shimansky <brodsm1487@gmail.com>
2022-12-14 00:31:59 +0200

    Create UserConfiguration.cs

---
+++ MyTodo.Storage/Configurations/UserConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyTodo.Storage.Models;
+
+namespace MyTodo.Storage.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Username)
+                .HasMaxLength(64)
+                .IsRequired();
+            builder.Property(x => x.FirstName)
+                .HasMaxLength(64)
+                .IsRequired();
+            builder.Property(x => x.LastName)
+                .HasMaxLength(64);
+
+            builder.Property(x => x.Email)
+                .HasMaxLength(254)
+                .IsRequired();
+
+            builder.Property(x => x.CreatedOn)
+                .IsRequired();
+        }
+    }
+}
