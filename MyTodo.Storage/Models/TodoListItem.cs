ed0fed7abb38e312629bb8c9bf0d66715b26f11e
Denis Shimansky <brodsm1487@gmail.com>
2022-12-14 00:32:10 +0200

    rename tasks

---
+++ MyTodo.Storage/Models/TodoListItem.cs
@@ -0,0 +1,17 @@
+﻿namespace MyTodo.Storage.Models
+{
+    public class TodoListItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public int? ParentId { get; set; }
+        public int TodoListId { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+        public bool IsDone { get; set; }
+
+        public TodoList? TodoList { get; set; }
+        public ICollection<TodoListItem>? Steps { get; set;}
+    }
+}
