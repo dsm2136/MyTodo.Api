845e71ee262962771a384ad956ca53715ac994b0
Denis Shimansky <brodsm1487@gmail.com>
2022-12-22 23:37:23 +0200

    Create DbContextFactory.cs

---
+++ MyTodo.Storage/DbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MyTodo.Storage
+{
+    internal class DbContextFactory : IDesignTimeDbContextFactory<TodoListDbContext>
+    {
+        public TodoListDbContext CreateDbContext(string[] args)
+        {
+            //var builder = new ConfigurationBuilder.
+            //    .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true)
+            //    .AddJsonFile("appSettings.Development.json", optional: true, reloadOnChange: true);
+
+            var optionsBuilder = new DbContextOptionsBuilder<TodoListDbContext>();
+            optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=MyTodo;User Id=sa;Password=roOt1234;TrustServerCertificate=True;");
+
+            return new TodoListDbContext(optionsBuilder.Options);
+        }
+    }
+}
